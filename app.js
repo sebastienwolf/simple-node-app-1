3661f7f2d3141d14625a979e8ead38140a16ba93
sebastienwolf <s.mouret.pro@gmail.com>
2022-10-06 09:46:32 +0200

    blabla

--- app.js
+++ app.js
@@ -1,39 +1,43 @@
 const http = require('http');
-const { Client } = require('pg')
+const { Client } = require('pg');
 
-const client = new Client()
-client.connect()
+const client = new Client();
+client.connect();
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
 process.on('SIGINT', (code) => {
   console.log('Process exit event with code: ', code);
-  client.end()
-  process.exit()
+  client.end();
+  process.exit();
 });
 
 // https://node-postgres.com/features/connecting
-var getMessageFromDB = function(cb){
-  client.query("SELECT * FROM messages", (err, res) => {
+const getMessageFromDB = function (cb) {
+  client.query('SELECT * FROM messages', (err, res) => {
     if (err) {
-      console.log(err.stack)
-      cb([])
+      console.log(err.stack);
+      cb([]);
     } else {
-      cb(res.rows)
+      cb(res.rows);
     }
-  })
-}
+  });
+};
 
 const server = http.createServer((req, res) => {
-  console.log("request received");
+  console.log('request received');
 
-  getMessageFromDB(function(messages){
-    html_text = "<ul>"
+  getMessageFromDB((messages) => {
+    // eslint-disable-next-line camelcase
+    let html_text = '<ul>';
+    // eslint-disable-next-line no-restricted-syntax
     for (const message of messages) {
-      html_text += "<li>" + message["text"] + "</li>"
+      // eslint-disable-next-line camelcase
+      html_text += `<li>${message.text}</li>`;
     }
-    html_text += "</ul>"
+    // eslint-disable-next-line camelcase
+    html_text += '</ul>';
 
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/html');
